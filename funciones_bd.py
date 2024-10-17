976c6ad885f565ef95f352e70baccc3c421dcd6f
Jhonatan <jdeangelb@gmail.com>
2024-10-17 07:48:46 -0500

    Add files via upload

--- funciones_bd.py
+++ funciones_bd.py
@@ -2,14 +2,32 @@ import mysql.connector
 
 class Data():
     def __init__(self):
+        self.crear_base_de_datos()
         self.cnn=mysql.connector.connect(host='localhost', user='root', password='psw@Sena2024', database='muen_prueba')
 
     def __str__(self):
-        datos = self.consultarUsuario()
+        datos = self.consultarUsuarios()
         aux = ''
         for fila in datos:
             aux = aux+str(fila)+'\n'
         return aux
+    
+    def crear_base_de_datos(self):
+        try:
+            connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="psw@Sena2024"
+            )
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS muen_prueba")
+            cursor.execute("USE muen_prueba")
+            cursor.execute("CREATE TABLE IF NOT EXISTS data (iddata INT AUTO_INCREMENT PRIMARY KEY, Usuario VARCHAR(45), Email VARCHAR(45))")
+            print("Base de datos verificada o creada")
+            connection.close()
+        except mysql.connector.Error as err:
+            print(f"Error al crear la base de datos: {err}")
+
     
     def consultarUsuarios(self):
         conex = self.cnn.cursor()
